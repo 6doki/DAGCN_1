b0a69dc016af7c0fd6f61a17a8e8214b33e4bbee
6doki <130281729+6doki@users.noreply.github.com>
2026-03-06 20:22:50 +0800

    Delete Data/PEMSD8 directory

--- Data/PEMSD8/read_data.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-
-data_file = "distance.csv"
-with open(data_file, 'r') as f:
-    data = pd.read_csv(f).values
-print(max(data[:, 0]))
-print(max(data[:, 1]))
